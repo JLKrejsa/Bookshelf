1e7c942b4305999e91613f5aaa2ec451d36fa703
Jennifer Krejsa <jlkrejsa@gmail.com>
2017-02-06 15:07:19 -0500

    second commit

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        //fields
+        [Key]
+        public int ID { get; set; }
+
+
+        public string Title { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+
+
+    }
+}
